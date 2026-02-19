c7224396692c9dcdcc7a9c1cbfe389b1dd82c39e
Fantom <ridwangani93@gmail.com>
2026-02-19 10:12:13 +0700

    First Push

---
+++ Assets/Scripts/Waypoint/Route2PointPosSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Route2PointPosSO", menuName = "Scriptable Objects/Route2PointPosSO")]
+public class Route2PointPosSO : ScriptableObject
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+}
